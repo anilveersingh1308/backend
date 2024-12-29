034807e4a4e3ee30ad1c96669cf0d1ed9fa9cdeb
Anil Singh <anilveersinghstudycode@gmail.com>
2024-12-29 17:03:53 +0530

    frontend inserted successfully

---
+++ frontend/src/app/page.js
@@ -0,0 +1,11 @@
+import Students from "@/pages/students";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Student Management</h1>
+      <Students />
+    </div>
+  );
+}
